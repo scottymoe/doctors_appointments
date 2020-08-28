19a81e51a192eb6b6af9780783042d3a0ce41f9b
Scott Mohorn <scottmohorn@gmail.com>
2020-08-28 11:31:41 -0600

    created models, add gems, created seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do |i|
+  p = Patient.create(first_name: Faker::Name.name,last_name: Faker::Name.last_name)
+  d = Doctor.create(last_name: "Dr #{Faker::Name.last_name}")
+end
+
+puts "seeded"
